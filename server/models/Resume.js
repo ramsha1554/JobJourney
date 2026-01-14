43beafa3a20431acb4ae27827d3c88f5a523ee7b
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-15 01:05:57 +0530

    server/models/Resume.js

---
+++ server/models/Resume.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ResumeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    name: {
+        type: String,
+        required: [true, 'Please add a name for this resume version'],
+        trim: true
+    },
+    filePath: {
+        type: String,
+        required: [true, 'Please add the file path']
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Resume', ResumeSchema);
